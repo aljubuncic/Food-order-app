5698e7f12608f0084c5b02363527d888ebab9e63
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-12 10:10:49 +0100

    add Order_Meal class

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Order_Meal.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Order_Meal {
+    private int idOrder_Meal;
+    private int idOrder;
+    private int idMeal;
+
+    public int getIdOrder_Meal() {
+        return idOrder_Meal;
+    }
+
+    public void setIdOrder_Meal(int idOrder_Meal) {
+        this.idOrder_Meal = idOrder_Meal;
+    }
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public int getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(int idMeal) {
+        this.idMeal = idMeal;
+    }
+}
