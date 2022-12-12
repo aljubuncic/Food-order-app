17bfb8b11e0466e8b6f330d0f6aff12aa9c2dd23
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-12 10:08:07 +0100

    add equals method

--- src/main/java/ba/unsa/etf/rpr/domain/Order.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Order.java
@@ -50,6 +50,13 @@ public class Order {
                 ", price=" + price +
                 '}';
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return idOrder == order.idOrder && idUser == order.idUser && Double.compare(order.price, price) == 0 && dateOfOrder.equals(order.dateOfOrder);
+    }
 
 
 }
