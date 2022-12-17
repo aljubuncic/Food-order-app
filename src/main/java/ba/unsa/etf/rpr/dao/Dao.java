78b3ad8ec8c989dd76df00fef951f8ae47447833
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-17 15:06:03 +0100

    add throw to method signature

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
 import java.util.List;
 
 /**
@@ -11,32 +13,32 @@ public interface Dao <Type>{
      * @param id primary key of entity
      * @return Entity from database
      */
-    Type getById(int id);
+    Type getById(int id) throws OrderException;
 
     /**
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    Type add(Type item);
+    Type add(Type item) throws OrderException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    Type update(Type item);
+    Type update(Type item) throws OrderException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
      */
-    void delete(int id);
+    void delete(int id) throws OrderException;
 
     /**
      * Lists all entities from database.
      * WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<Type> getAll();
+    List<Type> getAll() throws OrderException;
 }
