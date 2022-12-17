d0b78674415dff77bdfd1612d4a71abe9270e1b0
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-17 13:35:06 +0100

    add getByDateRange

--- src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
@@ -1,6 +1,20 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Order;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
+import java.util.Date;
+import java.util.List;
 
 public interface OrderDao extends Dao<Order>{
+
+    /**
+     * Returns all orders within date range
+     * @param startingDate
+     * @param endingDate
+     * @return lis of Orders
+     * @throws OrderException
+     */
+
+    List<Order> getByDateRange(Date startingDate, Date endingDate) throws OrderException;
 }
