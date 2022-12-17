130bd5602b6da854f0728b2552ff8aca272d3035
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-17 13:04:06 +0100

    add getByEmail

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,10 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
+import java.util.List;
 
 /**
  * Dao interface for User domain bean
  */
 
 public interface UserDao extends Dao<User>{
+    /**
+     * Returns user with specified email
+     * @param email
+     * @return User
+     * @throws OrderException
+     */
+    User getByEmail(String email) throws OrderException;
+
+
 }
