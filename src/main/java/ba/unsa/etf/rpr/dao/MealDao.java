53fe848949b9be2d54679c9361a5332f3a092cad
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-17 13:19:52 +0100

    add MealDao class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MealDao.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
+import java.util.List;
+
+/**
+ * Dao interface for Meal domain bean
+ */
+
+public interface MealDao extends Dao<Meal>{
+
+
+}
