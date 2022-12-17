2c45984f296f61e80d47d5d03b644e944a89ed7a
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-17 14:57:21 +0100

    update Identifiable interface

--- src/main/java/ba/unsa/etf/rpr/domain/Identifiable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Identifiable.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * Interface that forces all beans of entities to have id field
+ */
+
 public interface Identifiable {
+
+    /**
+     * Sets id of an entity
+     * @param id
+     */
+
+    void setId(int id);
+
+    /**
+     * Returns id of an entty
+     * @return id
+     */
+
+    int getId();
 }
