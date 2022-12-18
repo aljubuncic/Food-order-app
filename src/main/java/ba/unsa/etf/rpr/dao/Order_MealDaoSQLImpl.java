65479b3b5fce204c2323c6a1ea6ed69b7baf7fc0
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-18 16:27:47 +0100

    implement rowToObject

--- src/main/java/ba/unsa/etf/rpr/dao/Order_MealDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Order_MealDaoSQLImpl.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.domain.Order_Meal;
 import ba.unsa.etf.rpr.exceptions.OrderException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Order_MealDaoSQLImpl extends AbstractDao<Order_Meal> implements Order_MealDao{
 
@@ -12,6 +13,14 @@ public class Order_MealDaoSQLImpl extends AbstractDao<Order_Meal> implements Ord
     }
     @Override
     public Order_Meal rowToObject(ResultSet rs) throws OrderException {
-        return null;
+        Order_Meal order_meal=new Order_Meal();
+        try {
+            order_meal.setId(rs.getInt("id"));
+            order_meal.setIdOrder(rs.getInt("idOrder"));
+            order_meal.setIdMeal(rs.getInt("idMeal"));
+            return order_meal;
+        } catch (SQLException e) {
+            throw new OrderException(e.getMessage());
+        }
     }
 }
