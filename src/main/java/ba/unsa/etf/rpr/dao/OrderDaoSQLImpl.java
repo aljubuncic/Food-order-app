0dcc6d99aff77a971c3c811b67d58aefa745ecdb
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-12-18 12:15:44 +0100

    implemented getByDateRange method

--- src/main/java/ba/unsa/etf/rpr/dao/OrderDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrderDaoSQLImpl.java
@@ -3,8 +3,10 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Order;
 import ba.unsa.etf.rpr.exceptions.OrderException;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +32,20 @@ public class OrderDaoSQLImpl extends AbstractDao<Order> implements OrderDao{
 
     @Override
     public List<Order> getByDateRange(Date startingDate, Date endingDate) throws OrderException {
-        return null;
+        try{
+            PreparedStatement statement=getConnection().prepareStatement("SELECT * FROM Order WHERE dateOfOrder BETWEEN ? AND ?");
+            statement.setObject(1,startingDate);
+            statement.setObject(2,endingDate);
+            ResultSet queryResult = statement.executeQuery();
+            List<Order> orders = new ArrayList<>();
+            while(queryResult.next()){
+                Order object = rowToObject(queryResult);
+                orders.add(object);
+            }
+            queryResult.close();
+            return orders;
+        }catch (SQLException e){
+            throw new OrderException(e.getMessage());
+        }
     }
 }
