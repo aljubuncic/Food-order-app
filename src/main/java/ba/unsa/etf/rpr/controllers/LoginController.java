25023f2000f8ad641718868a87d283f63076b8de
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-01-02 14:41:23 +0100

    add LoginController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public Button loginButton;
+
+    public void loginClick(ActionEvent actionEvent) {
+    }
+
+}
