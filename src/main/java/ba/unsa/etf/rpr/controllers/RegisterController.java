4c44af058bbbddca81628efe19dff2277bce8c4b
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-01-02 14:50:43 +0100

    add RegisterController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class RegisterController {
+
+    public TextField nameField;
+    public TextField surnameField;
+    public TextField usernameField;
+    public TextField emailField;
+    public TextField telephoneNumberField;
+    public TextField addressField;
+    public TextField passwordField;
+    public TextField confirmedPasswordField;
+    public Button registerButton;
+
+    public void registerClick(ActionEvent actionEvent) {
+    }
+
+}
