36e6b6faeb91c6d24b62a82f8877ea3216f5d101
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-01-31 18:34:44 +0100

    add validation to login credentials from database

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -1,8 +1,23 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
 /**
  * Business Logic Layer for management of Users
  */
 
 public class UserManager {
+    public void validateLoginCredentials(String username, String password) throws OrderException {
+        User user;
+        try{
+            user = DaoFactory.userDao().getByUsername(username);
+        }
+        catch(OrderException e){
+            throw new OrderException("User with username " + username + " does not exist");
+        }
+        if(!user.getPassword().equals(password))
+            throw new OrderException("Incorrect password");
+    }
 }
