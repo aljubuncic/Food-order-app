d69d4410000bde41d2f0d364b47959086e47ba72
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-01-31 16:36:31 +0100

    add validation to login credentials

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,17 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import com.mysql.cj.xdevapi.Schema;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class LoginController {
+
+    private UserManager userManager = new UserManager();
 
     private void closeLoginWindow(ActionEvent actionEvent){
         Node node = (Node) actionEvent.getSource();
@@ -19,11 +24,77 @@ public class LoginController {
         oldStage.close();
     }
 
+    private void setUsernameInvalid(){
+        usernameField.getStyleClass().add("fieldIsEmpty");
+        checkUsername.setText("Username is empty");
+    }
+    private void setUsernameValid(){
+        usernameField.getStyleClass().removeAll("fieldIsEmpty");
+        checkUsername.setText("");
+    }
+    private void setPasswordInvalid(){
+        passwordField.getStyleClass().add("fieldIsEmpty");
+        checkPassword.setText("Password is empty");
+    }
+    private void setPasswordValid(){
+        passwordField.getStyleClass().removeAll("fieldIsEmpty");
+        checkPassword.setText("");
+    }
+
+
     public TextField usernameField;
     public PasswordField passwordField;
     public Button loginButton;
 
+    public Label checkUsername;
+    public Label checkPassword;
+    @FXML
+    public void initialize(){
+        usernameField.textProperty().addListener(((observableValue, oldUsername, newUsername) -> {
+            if(newUsername.isEmpty()) {
+                setUsernameInvalid();
+            }
+            else {
+                setUsernameValid();
+            }
+        }));
+
+        passwordField.textProperty().addListener(((obs, oldPassword, newPassword) -> {
+            if(newPassword.isEmpty()) {
+                setPasswordInvalid();
+            }
+            else {
+                setPasswordValid();
+            }
+        }));
+    }
+
     public void loginClick(ActionEvent actionEvent) {
+        if(usernameField.getText().isEmpty()){
+            usernameField.requestFocus();
+            setUsernameInvalid();
+            if(passwordField.getText().isEmpty())
+                setPasswordInvalid();
+            return;
+        } else if (passwordField.getText().isEmpty()) {
+            passwordField.requestFocus();
+            setPasswordInvalid();
+            return;
+        }
+
+        try {
+            userManager.validateLoginCredentials(usernameField.getText(),passwordField.getText());
+        } catch (OrderException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login error");
+            alert.setHeaderText("Login credentials are not valid");
+            alert.setContentText(e.getMessage());
+            if(e.getMessage().equals("Incorrect password"))
+                passwordField.requestFocus();
+            else
+                usernameField.requestFocus();
+            alert.showAndWait();
+        }
     }
 
     public void switchToRegisterWindow(ActionEvent actionEvent) throws Exception{
