e86d8798d5720a02af67fa4155f37d5c143d0e7f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-02 00:16:42 +0100

    add methods for validation of register credentials

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -18,6 +20,43 @@ public class RegisterController {
         Stage oldStage = (Stage) node.getScene().getWindow();
         oldStage.close();
     }
+    private void setFieldInvalid(TextField textField, Label checkLabel, String errorMessage) {
+            textField.getStyleClass().add("fieldIsEmpty");
+            checkLabel.setText(errorMessage);
+    }
+    private void setFieldValid(TextField textField, Label checkLabel) {
+        textField.getStyleClass().removeAll("fieldIsEmpty");
+        checkLabel.setText("");
+    }
+    private boolean matchesRegex(String n,String regex,TextField textField, Label checkLabel,String fieldName,String errorMessage){
+        if(!n.matches(regex)) {
+            if (n.isEmpty()) {
+                setFieldInvalid(textField, checkLabel,  fieldName + " is empty");
+            } else {
+                setFieldInvalid(textField, checkLabel, errorMessage);
+            }
+            return false;
+        }
+        return true;
+    }
+    private void addListenerToField(TextField textField, Label checkLabel, String errorMessage,String fieldName,String regex){
+        textField.textProperty().addListener(((observableValue, o, n) -> {
+            if(matchesRegex(n,regex,textField,checkLabel,fieldName,errorMessage))
+                setFieldValid(textField,checkLabel);
+        }));
+    }
+    private void setFieldInvalidAfterClick(TextField textField, Label checkLabel, String errorMessage){
+        textField.requestFocus();
+        setFieldInvalid(textField,checkLabel,errorMessage);
+    }
+
+    public Label checkName;
+    public Label checkSurname;
+    public Label checkUsername;
+    public Label checkEmail;
+    public Label checkTelephoneNumber;
+    public Label checkPassword;
+    public Label checkConfirmedPassword;
     public TextField nameField;
     public TextField surnameField;
     public TextField usernameField;
