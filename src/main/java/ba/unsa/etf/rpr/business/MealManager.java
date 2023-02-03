ff8d0535e0468d91a004cdc7cd60f5307192f68f
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-03 20:48:40 +0100

    add getAll method

--- src/main/java/ba/unsa/etf/rpr/business/MealManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MealManager.java
@@ -1,7 +1,13 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.exceptions.OrderException;
 
+import java.util.List;
 
 public class MealManager {
-
+    public List<Meal> getAll() throws OrderException {
+        return DaoFactory.mealDao().getAll();
+    }
 }
