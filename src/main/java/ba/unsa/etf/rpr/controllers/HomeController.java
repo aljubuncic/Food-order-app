3b978a790165391acf4ad372005670184c28fefa
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-03 20:53:27 +0100

    implement fetching meals from database into the table

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,23 +1,43 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.MealManager;
 import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.domain.TypeOfMeal;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.OrderException;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class HomeController {
-    private UserManager userManager= new UserManager();
+    private final UserManager userManager= new UserManager();
+    private final MealManager mealManager = new MealManager();
     public Label usernameLabel;
     public Label nameLabel;
     public Label surnameLabel;
     private String username;
+    @FXML
+    private TableView<Meal> mealsTable;
+    @FXML
+    private TableColumn<Meal,String> nameColumn;
+    @FXML
+    private TableColumn<Meal, Double> priceColumn;
+    @FXML
+    private TableColumn<Meal,Integer> quantityColumn;
+    @FXML
+    private TableColumn<Meal, String> typeColumn;
 
     public HomeController(String username) {
         this.username = username;
@@ -25,6 +45,12 @@ public class HomeController {
 
     @FXML
     public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getType().toString()));
+        refreshMeals();
+
         usernameLabel.setText(username);
         try {
             User user = userManager.getByUsername(username);
@@ -32,6 +58,18 @@ public class HomeController {
             surnameLabel.setText(user.getSurname());
         } catch (OrderException e) {
             throw new RuntimeException(e);
+        }
+    }
+    /**
+     * fetches meals from database
+     */
+    private void refreshMeals(){
+        try{
+            mealsTable.setItems(FXCollections.observableList(mealManager.getAll()));
+            mealsTable.refresh();
+        }
+        catch(OrderException e){
+            new Alert(Alert.AlertType.ERROR,e.getMessage(), ButtonType.CLOSE).showAndWait();
         }
     }
     public void clickAbout(ActionEvent actionEvent) throws Exception{
