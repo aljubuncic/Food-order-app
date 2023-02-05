8d1e7630db6e0d2b31fa8b3bcbd61d3f397ec9b9
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-05 02:12:52 +0100

    add ConfirmOrderController class, implement initialize method

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/ConfirmOrderController.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.domain.User;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class ConfirmOrderController {
+    private double priceOfOrder = 0;
+    public RadioButton deliveryRadioButton;
+    public RadioButton sendEmailRadioButton;
+    public TextField addressField;
+    public TextField emailField;
+    public ListView cartList;
+    public Label priceOfOrderLabel;
+    public Label addressLabel;
+    public Label emailLabel;
+    private List<Meal> orderList;
+    private User user;
+
+    public ConfirmOrderController(List<Meal> selectedMeals, User user){
+        this.orderList = selectedMeals;
+        this.user = user;
+    }
+    @FXML
+    public void initialize(){
+        if(user.getAddress()!=null)
+            addressField.setText(user.getAddress());
+        if(user.getEmail()!=null)
+            emailField.setText(user.getEmail());
+        for(Meal meal : orderList){
+            priceOfOrder+= meal.getPrice();
+            StringBuilder builder = new StringBuilder();
+            builder.append(meal.getName()).append(" ").append(meal.getQuantity()).append("gr ").append(meal.getPrice()).append(" KM");
+            cartList.getItems().add(builder.toString());
+        }
+        priceOfOrderLabel.setText(priceOfOrder + " KM");
+    }
+}
