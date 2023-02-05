73331879556ea6c222d057766512741effd75cea
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-05 18:09:16 +0100

    add confirmationEmail and address attributes

--- src/main/java/ba/unsa/etf/rpr/domain/Order.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Order.java
@@ -12,6 +12,8 @@ public class Order implements Identifiable{
     private User user;
     private Date dateOfOrder;
     private double price;
+    private String confirmationEmail;
+    private String address;
 
     public int getId() {
         return id;
@@ -45,24 +47,44 @@ public class Order implements Identifiable{
         this.price = price;
     }
 
+    public String getConfirmationEmail() {
+        return confirmationEmail;
+    }
+
+    public void setConfirmationEmail(String confirmationEmail) {
+        this.confirmationEmail = confirmationEmail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "Order{" +
-                "idOrder=" + id +
-                ", idUser=" + user +
+                "id=" + id +
+                ", user=" + user +
                 ", dateOfOrder=" + dateOfOrder +
                 ", price=" + price +
+                ", confirmationEmail='" + confirmationEmail + '\'' +
+                ", address='" + address + '\'' +
                 '}';
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        return id == order.id && user == order.user && Double.compare(order.price, price) == 0 && dateOfOrder.equals(order.dateOfOrder);
+        return id == order.id && Double.compare(order.price, price) == 0 && user.equals(order.user) && dateOfOrder.equals(order.dateOfOrder) && Objects.equals(confirmationEmail, order.confirmationEmail) && Objects.equals(address, order.address);
     }
+
     @Override
     public int hashCode() {
-        return Objects.hash(id, user, dateOfOrder, price);
+        return Objects.hash(id, user, dateOfOrder, price, confirmationEmail, address);
     }
 }
