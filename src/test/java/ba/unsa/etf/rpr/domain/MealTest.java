0268b3adc21f8a74f207be3732b4be733faad93c
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:20:06 +0100

    add equalsMethodTest

--- src/test/java/ba/unsa/etf/rpr/domain/MealTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/MealTest.java
@@ -7,5 +7,14 @@ import org.junit.jupiter.api.Test;
  * Test class for Meal
  */
 public class MealTest {
+    /**
+     * This method tests equals method
+     */
+    @Test
+    void equalsMethodTest(){
+        Meal meal1 = new Meal("Pizza",4,250,"Main dish");
+        Meal meal2 = new Meal("Pizza",4,250,"Main dish");
+        Assertions.assertTrue(meal1.equals(meal2));
+    }
 
 }
