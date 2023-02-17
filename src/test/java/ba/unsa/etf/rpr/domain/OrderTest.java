5717d5aab537dff7e50c3a463d58469536168978
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:22:53 +0100

    add OrderTest

---
+++ src/test/java/ba/unsa/etf/rpr/domain/OrderTest.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+/**
+ * Test class for Order
+ */
+public class OrderTest {
+
+}
