e6e711f8750168a050b3aba7f95cf348679f3157
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 19:54:02 +0100

    add javadoc for OrderException

--- src/main/java/ba/unsa/etf/rpr/exceptions/OrderException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/OrderException.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * Exception that occurs when there is some fault with operations with the OrderDatabase
+ */
 public class OrderException extends Exception{
 
     public OrderException(String message) {
