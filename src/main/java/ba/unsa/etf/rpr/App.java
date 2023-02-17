62f43508a8c660e436281ac4a90a36ce87a8ecb4
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 17:31:30 +0100

    add variables for describing options on CLI

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,9 +1,32 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.MealManager;
+import ba.unsa.etf.rpr.business.OrderManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.domain.Order;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import org.apache.commons.cli.*;
+
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Class for CLI (Command Line Interface) implementation
+ */
 public class App
 {
+    private static final Option register = new Option("r","register",false,"Register - enter your name, surname, username, password and telephone number");
+    private static final Option displayMeals = new Option("m","display-meals",false,"Display all available meals");
+    private static final Option order = new Option("o","order",false,"Add new order to OrderDatabase (first and second argument are username and password, the rest of arguments are IDs of meals)");
 
-    public static void main(String[] args) {
-    }
+    private static final UserManager userManager = new UserManager();
+    private static final MealManager mealManager = new MealManager();
+    private static final OrderManager orderManager = new OrderManager();
+
+
 
 }
