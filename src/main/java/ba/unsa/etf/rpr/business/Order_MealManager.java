ea757e74f6d74b67a47a0b90158436c62befde5b
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:54:06 +0100

    add javadoc for Order_MealManager

--- src/main/java/ba/unsa/etf/rpr/business/Order_MealManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/Order_MealManager.java
@@ -12,12 +12,20 @@ import java.util.List;
  */
 
 public class Order_MealManager {
-    public List<Meal> getMealsFromOrder(Order order) throws OrderException {
-        return DaoFactory.order_MealDao().getMealsFromOrder(order);
-    }
+    /**
+     * Deletes all rows in the database associated with the order
+     * @param order Order object
+     * @throws OrderException
+     */
     public void deleteOrder(Order order) throws OrderException{
         DaoFactory.order_MealDao().deleteOrder(order.getId());
     }
+
+    /**
+     * Deletes all rows in the database associated with the meal
+     * @param meal Meal object
+     * @throws OrderException
+     */
     public void deleteMeal(Meal meal) throws OrderException{
         DaoFactory.order_MealDao().deleteMeal(meal.getId());
     }
