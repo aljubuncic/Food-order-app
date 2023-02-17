ae43086b2cd3d96243d05f99b76b70fc0f67babf
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:23:57 +0100

    add equalsMethodTest

--- src/test/java/ba/unsa/etf/rpr/domain/OrderTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/OrderTest.java
@@ -9,5 +9,16 @@ import java.util.Date;
  * Test class for Order
  */
 public class OrderTest {
+    /**
+     * This method tests equals method
+     */
+    @Test
+    void equalsMethodTest(){
+        User user = new User("name","surname","username","password","007");
+        Date date = new Date();
+        Order order1 = new Order(user,date,20);
+        Order order2 = new Order(user,date,20);
+        Assertions.assertTrue(order1.equals(order2));
+    }
 
 }
