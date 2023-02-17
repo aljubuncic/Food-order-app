95f3006d0f480847ef62d40ae097c3b4801092d4
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:32:31 +0100

    add MealManagerTest

---
+++ src/test/java/ba/unsa/etf/rpr/business/MealManagerTest.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.MealDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+/**
+ * Test class for MealManager
+ */
+public class MealManagerTest {
+
+}
