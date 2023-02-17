39a70821502591e19c6c5d4d012e204228edc404
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:40:37 +0100

    add UserManagerTest

---
+++ src/test/java/ba/unsa/etf/rpr/business/UserManagerTest.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test class for UserManager
+ */
+public class UserManagerTest {
+
+}
