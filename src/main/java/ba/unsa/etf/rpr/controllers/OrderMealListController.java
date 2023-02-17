17fc9630c7a828c2f0133dd4745b8e581e20bddb
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 19:45:08 +0100

    modify javadoc for OrderMealListController

--- src/main/java/ba/unsa/etf/rpr/controllers/OrderMealListController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OrderMealListController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.ListView;
 
 import java.util.List;
 
+/**
+ * JavaFX Controller for window displaying meals in the order list
+ */
 public class OrderMealListController extends AbstractController{
     @FXML
     public ListView orderListView;
@@ -14,6 +17,10 @@ public class OrderMealListController extends AbstractController{
     public OrderMealListController(List<Meal> orderList){
         this.orderList=orderList;
     }
+
+    /**
+     * Populates a ListView with order list
+     */
     @FXML
     public void initialize(){
         for(Meal meal : orderList)
