990a3bd3b590498da9917398a2a0b1541f2d9501
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 17:32:06 +0100

    add method printFormattedOptions

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -27,6 +27,17 @@ public class App
     private static final MealManager mealManager = new MealManager();
     private static final OrderManager orderManager = new OrderManager();
 
+    /**
+     * Prints all the options on the console
+     * @param options - options to be printed
+     */
+    public static void printFormattedOptions(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        PrintWriter printWriter = new PrintWriter(System.out);
+        helpFormatter.printUsage(printWriter, 150, "java -jar Food-order-app.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
+        printWriter.close();
+    }
 
 
 }
