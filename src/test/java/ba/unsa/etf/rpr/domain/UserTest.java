e271419581793cc04282306b52fd9faa83f61323
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:27:31 +0100

    add toStringMethodTest

--- src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
@@ -16,4 +16,13 @@ public class UserTest {
         User user2 = new User("name","surname","username","password","007");
         Assertions.assertTrue(user1.equals(user2));
     }
+    /**
+     * This method tests toString method
+     */
+    @Test
+    void toStringMethodTest(){
+        User user = new User("name","surname","username","password","007");
+        String string = "User{id=0, name='name', surname='surname', username=username, email='null', password='password', address='null', telephoneNumber='007'}";
+        Assertions.assertEquals(user.toString(), string);
+    }
 }
