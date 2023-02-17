cc1ca49ea297e93bf9f4bdc70961ceef444628fa
Ahmed Ljubunčić <aljubuncic1@etf.unsa.ba>
2023-02-17 18:37:24 +0100

    add OrderManagerTest

---
+++ src/test/java/ba/unsa/etf/rpr/business/OrderManagerTest.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.MealDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.OrderDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Meal;
+import ba.unsa.etf.rpr.domain.Order;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Test class for OrderManager
+ */
+public class OrderManagerTest {
+
+}
